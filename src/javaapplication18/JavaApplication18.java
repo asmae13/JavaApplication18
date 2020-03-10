08bddf89f6732c0aa358db83abcb444782c6adff
asmae13 <61909456+asmae13@users.noreply.github.com>
2020-03-10 09:58:26 +0100

    Update JavaApplication18.java

--- src/javaapplication18/JavaApplication18.java
+++ src/javaapplication18/JavaApplication18.java
@@ -5,16 +5,7 @@
  */
 package javaapplication18;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import org.apache.jena.ontology.Individual;
-import org.apache.jena.ontology.OntClass;
-import org.apache.jena.ontology.OntModel;
-import org.apache.jena.ontology.OntModelSpec;
-import org.apache.jena.ontology.OntProperty;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.RDFWriter;
+
 /**
 
 /**
@@ -26,25 +17,7 @@ public class JavaApplication18 {
     /**
      * @param args the command line arguments
      */
-	public void createOnto() throws FileNotFoundException{
-	    OntModel model=ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM);
-	    String exns = "http://www.example.com/vocabulary#";
-	    model.setNsPrefix("ns",exns);
-	    //creation des classes
-	    OntClass hum = model.createClass(exns+"Humain");
-	    OntClass homme = model.createClass(exns+ "Homme");
-	    OntClass femme = model.createClass(exns+ "Femme");
-	    OntClass etudiant = model.createClass(exns+ "Etudiant");
-	    OntClass enseignant = model.createClass(exns+ "Enseignat");
-	    OntClass etudGrade = model.createClass(exns+ "Etudiant_en_graduation");
-	    OntClass etudPostGrade = model.createClass(exns+ "Etudiant_en_post_graduation");
-	    OntClass maitreAssist = model.createClass(exns+ "maitre_assistant");
-	    OntClass maitreConf = model.createClass(exns+ "maitre_de_Conferences");
-	    OntClass professeur = model.createClass(exns+ "professeur");
-	    OntClass cours=model.createClass(exns+ "cours");
-	    OntClass promo=model.createClass(exns+ "Promo");
-	    
-	}
+	
     public static void main(String[] args) {
         // TODO code application logic here
     }
